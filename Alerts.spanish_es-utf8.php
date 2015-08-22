fe26677cfc4c5b1a8209a5644c38274d39e6181b
Marcos Del Sol Vives <socram8888@gmail.com>
2015-08-22 23:59:20 +0200

    Traducción de avisos

--- Alerts.spanish_es-utf8.php
+++ Alerts.spanish_es-utf8.php
@@ -2,45 +2,45 @@
 // Version: 2.1 Beta 2; Alerts
 
 // Load Alerts strings
-$txt['topic_na'] = '(private topic)';
-$txt['board_na'] = '(private board)';
+$txt['topic_na'] = '(tema privado)';
+$txt['board_na'] = '(foro privado)';
 
-$txt['all_alerts'] = 'All alerts';
-$txt['mark_alerts_read'] = 'Mark read';
-$txt['alert_settings'] = 'Settings';
-$txt['alerts_no_unread'] = 'No unread alerts.';
+$txt['all_alerts'] = 'Todos los avisos';
+$txt['mark_alerts_read'] = 'Marcar como leídos';
+$txt['alert_settings'] = 'Opciones';
+$txt['alerts_no_unread'] = 'No hay avisos sin leer.';
 
-$txt['alert_topic_reply'] = '{member_link} replied to the topic {topic_msg}';
-$txt['alert_topic_move'] = 'The topic {topic_msg} has been moved to {board_msg}';
-$txt['alert_topic_remove'] = 'The topic {topic_msg} has been deleted';
-$txt['alert_topic_unlock'] = 'The topic {topic_msg} has been unlocked';
-$txt['alert_topic_lock'] = 'The topic {topic_msg} has been locked';
-$txt['alert_topic_split'] = 'The topic {topic_msg} has been split';
-$txt['alert_topic_merge'] = 'One or more topics have been merged into {topic_msg}';
-$txt['alert_topic_sticky'] = 'The topic {topic_msg} has been stickied';
-$txt['alert_board_topic'] = '{member_link} started a new topic {topic_msg} in {board_msg}';
-$txt['alert_unapproved_topic'] = '{member_link} started a new unapproved topic {topic_msg} in {board_msg}';
-$txt['alert_unapproved_post'] = '{member_link} made a new unapproved post {topic_msg} in {board_msg}';
-$txt['alert_unapproved_reply'] = '{member_link} replied to your unapproved topic {topic_msg} in {board_msg}';
-$txt['alert_msg_quote'] = '{member_link} quoted you in the post {msg_msg}';
-$txt['alert_msg_mention'] = '{member_link} mentioned you in the post {msg_msg}';
-$txt['alert_msg_like'] = '{member_link} liked your post {msg_msg}';
-$txt['alert_msg_report'] = '{member_link} <a href="{scripturl}{report_link}">reported a post</a> - {msg_msg}';
-$txt['alert_msg_report_reply'] = '{member_link} replied to <a href="{scripturl}{report_link}">the report</a> about {msg_msg}';
-$txt['alert_profile_report'] = '{member_link} <a href="{scripturl}{report_link}">reported</a> the profile of {profile_msg}';
-$txt['alert_profile_report_reply'] = '{member_link} replied to <a href="{scripturl}{report_link}">the report</a> about the profile of {profile_msg}';
-$txt['alert_member_register_standard'] = '{member_link} just signed up';
-$txt['alert_member_register_approval'] = '{member_link} just signed up (account requires approval)';
-$txt['alert_member_register_activation'] = '{member_link} just signed up (account requires activation)';
-$txt['alert_member_group_request'] = '{member_link} has <a href="{scripturl}?action=moderate;area=groups;sa=requests">requested</a> to join {group_name}';
-$txt['alert_groupr_approved'] = 'Your request to join {group_name} has been approved';
-$txt['alert_groupr_rejected'] = 'Your request to join {group_name} has been rejected{reason}';
-$txt['alert_event_new_guest'] = 'A new event, <a href="{scripturl}?action=calendar;event={event_id}">{event_title}</a>, was added to the calendar.';
-$txt['alert_event_new'] = '{member_link} added a new event, <a href="{scripturl}?action=calendar;event={event_id}">{event_title}</a> to the calendar.';
-$txt['alert_event_new_topic'] = '{member_link} added a new event, {topic_msg}, to the calendar.';
-$txt['alert_paidsubs_expiring'] = 'Your subscription to <a href="{scripturl}?action=profile;area=subscriptions">{subscription_name}</a> is about to expire at {end_time}';
-$txt['alert_buddy_buddy_request'] = '{member_link} added you as their buddy';
+$txt['alert_topic_reply'] = '{member_link} ha respondido en el tema {topic_msg}';
+$txt['alert_topic_move'] = 'El tema {topic_msg} ha sido movido a {board_msg}';
+$txt['alert_topic_remove'] = 'El tema {topic_msg} ha sido eliminado';
+$txt['alert_topic_unlock'] = 'El tema {topic_msg} ha sido reabierto';
+$txt['alert_topic_lock'] = 'El tema {topic_msg} ha sido cerrado';
+$txt['alert_topic_split'] = 'El tema {topic_msg} ha sido dividido';
+$txt['alert_topic_merge'] = 'Dos o más temas han sido unidos en {topic_msg}';
+$txt['alert_topic_sticky'] = 'El tema {topic_msg} está ahora fijado';
+$txt['alert_board_topic'] = '{member_link} ha creado el tema {topic_msg} en {board_msg}';
+$txt['alert_unapproved_topic'] = '{member_link} ha creado el tema {topic_msg}, no aprobado, en {board_msg}';
+$txt['alert_unapproved_post'] = '{member_link} ha escrito un nuevo post {topic_msg}, no aprobado, en {board_msg}';
+$txt['alert_unapproved_reply'] = '{member_link} ha respondido en tu tema no aprobado {topic_msg} en {board_msg}';
+$txt['alert_msg_quote'] = '{member_link} te ha citado en el mensaje {msg_msg}';
+$txt['alert_msg_mention'] = '{member_link} te ha mencionado en el mensaje {msg_msg}';
+$txt['alert_msg_like'] = 'A {member_link} le gusta tu mensaje {msg_msg}';
+$txt['alert_msg_report'] = '{member_link} <a href="{scripturl}{report_link}">ha reportado un mensaje</a> - {msg_msg}';
+$txt['alert_msg_report_reply'] = '{member_link} ha respondido a <a href="{scripturl}{report_link}">tu reporte</a> sobre {msg_msg}';
+$txt['alert_profile_report'] = '{member_link} <a href="{scripturl}{report_link}">ha reportado</a> el perfil de {profile_msg}';
+$txt['alert_profile_report_reply'] = '{member_link} ha respondido a <a href="{scripturl}{report_link}">tu reporte</a> sobre el perfil de {profile_msg}';
+$txt['alert_member_register_standard'] = '{member_link} acaba de registrarse';
+$txt['alert_member_register_approval'] = '{member_link} acaba de registrarse (la cuenta debe ser aprobada)';
+$txt['alert_member_register_activation'] = '{member_link} acaba de registrarse (la cuenta debe ser activada)';
+$txt['alert_member_group_request'] = '{member_link} ha <a href="{scripturl}?action=moderate;area=groups;sa=requests">solicitado</a> unirse a {group_name}';
+$txt['alert_groupr_approved'] = 'Tu solicitud de ingreso a {group_name} ha sido aprobada';
+$txt['alert_groupr_rejected'] = 'Tu solicitud de ingreso a {group_name} ha sido rechazada{reason}';
+$txt['alert_event_new_guest'] = 'Un nuevo evento, <a href="{scripturl}?action=calendar;event={event_id}">{event_title}</a>, ha sido añadido al calendario.';
+$txt['alert_event_new'] = '{member_link} ha añadido un nuevo evento (<a href="{scripturl}?action=calendar;event={event_id}">{event_title}</a>) al calendario.';
+$txt['alert_event_new_topic'] = '{member_link} ha añadido un nuevo evento ({topic_msg}) al calendario.';
+$txt['alert_paidsubs_expiring'] = 'Tu suscripción a <a href="{scripturl}?action=profile;area=subscriptions">{subscription_name}</a> expirará en {end_time}';
+$txt['alert_buddy_buddy_request'] = '{member_link} te ha añadido como amigo';
 $txt['alert_birthday_mag'] = '{happy_birthday}';
-$txt['alerts_none'] = 'You have no alerts.';
+$txt['alerts_none'] = 'No tienes avisos.';
 
 ?>
